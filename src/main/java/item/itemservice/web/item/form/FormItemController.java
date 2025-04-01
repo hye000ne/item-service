f4dccca820309aa1f052ae4f673d1c9875c5ff93
hye000ne <_hye_one@hanmail.net>
2025-04-02 00:38:00 +0900

    radio/select box add

--- src/main/java/item/itemservice/web/item/form/FormItemController.java
+++ src/main/java/item/itemservice/web/item/form/FormItemController.java
@@ -1,7 +1,9 @@
 package item.itemservice.web.item.form;
 
+import item.itemservice.domain.item.DeliveryCode;
 import item.itemservice.domain.item.Item;
 import item.itemservice.domain.item.ItemRepository;
+import item.itemservice.domain.item.ItemType;
 import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -10,6 +12,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import java.util.ArrayList;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
@@ -22,13 +25,28 @@ public class FormItemController {
     private final ItemRepository itemRepository;
 
     @ModelAttribute("regions")
-    private Map<String, String> regions() {
+    public Map<String, String> regions() {
         Map<String, String> regions = new LinkedHashMap<>();
         regions.put("SEOUL", "서울");
         regions.put("BUSAN", "부산");
         regions.put("JEJU", "제주");
 
         return regions;
+    }
+
+    @ModelAttribute("itemTypes")
+    public ItemType[] itemTypes(){
+        return ItemType.values();
+    }
+
+    @ModelAttribute("deliveryCodes")
+    public List<DeliveryCode> deliveryCodes(){
+        List<DeliveryCode> deliveryCodes = new ArrayList<>();
+        deliveryCodes.add(new DeliveryCode("FAST","빠른 배송"));
+        deliveryCodes.add(new DeliveryCode("NORMAL","일반 배송"));
+        deliveryCodes.add(new DeliveryCode("SLOW","느린 배송"));
+
+        return deliveryCodes;
     }
 
     @GetMapping
@@ -58,6 +76,8 @@ public class FormItemController {
     public String add(@ModelAttribute Item item, RedirectAttributes redirectAttributes){
         log.info("item.open={}", item.getOpen());
         log.info("item.regions={}", item.getRegions());
+        log.info("item.itemTypes={}", item.getItemType());
+        log.info("item.deliveryCode={}", item.getDeliveryCode());
 
         Item saveditem = itemRepository.save(item);
         redirectAttributes.addAttribute("itemId", saveditem.getId());
